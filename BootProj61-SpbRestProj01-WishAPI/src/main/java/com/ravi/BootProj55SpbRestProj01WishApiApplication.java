59f789acc5c77465313077b34575c37848b830a5
Ravi kumar <rsravi433399@gmail.com>
2026-02-26 18:25:12 +0530

    Spring rest 1st project WishAPI added

---
+++ BootProj61-SpbRestProj01-WishAPI/src/main/java/com/ravi/BootProj55SpbRestProj01WishApiApplication.java
@@ -0,0 +1,13 @@
+package com.ravi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootProj55SpbRestProj01WishApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootProj55SpbRestProj01WishApiApplication.class, args);
+	}
+
+}
---
+++ BootProj61-SpbRestProj01-WishAPI/src/test/java/com/ravi/BootProj55SpbRestProj01WishApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootProj55SpbRestProj01WishApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
