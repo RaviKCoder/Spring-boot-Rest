63df27c2d944bff111496c443b37af0440fffa38
Ravi kumar <rsravi433399@gmail.com>
2026-02-27 23:48:19 +0530

    Rest project2 added

---
+++ BootProj62-SpbRestProj02-AllModeOfRequestMethod/src/main/java/com/ravi/BootProj62SpbRestProj02AllModeOfRequestMethodApplication.java
@@ -0,0 +1,13 @@
+package com.ravi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootProj62SpbRestProj02AllModeOfRequestMethodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootProj62SpbRestProj02AllModeOfRequestMethodApplication.class, args);
+	}
+
+}
