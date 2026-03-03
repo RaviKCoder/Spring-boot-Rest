eaa3584a4d077e8da60a85ad60ac9ab32e7b567f
Ravi kumar <rsravi433399@gmail.com>
2026-03-04 02:39:48 +0530

    Spb Rest project7 is added

---
+++ BootProj67-SpbRestProj07-RequestParams/src/main/java/com/ravi/BootProj67SpbRestProj07RequestParamsApplication.java
@@ -0,0 +1,13 @@
+package com.ravi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootProj67SpbRestProj07RequestParamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootProj67SpbRestProj07RequestParamsApplication.class, args);
+	}
+
+}
