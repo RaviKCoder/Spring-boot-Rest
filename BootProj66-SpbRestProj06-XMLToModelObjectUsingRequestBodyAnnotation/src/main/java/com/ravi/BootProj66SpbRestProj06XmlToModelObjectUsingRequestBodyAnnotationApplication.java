1bab0e5635d0bccddc19f6ce352a84ca375afb94
Ravi kumar <rsravi433399@gmail.com>
2026-03-03 15:09:58 +0530

    Spb Rest project6 is added

---
+++ BootProj66-SpbRestProj06-XMLToModelObjectUsingRequestBodyAnnotation/src/main/java/com/ravi/BootProj66SpbRestProj06XmlToModelObjectUsingRequestBodyAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.ravi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootProj66SpbRestProj06XmlToModelObjectUsingRequestBodyAnnotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootProj66SpbRestProj06XmlToModelObjectUsingRequestBodyAnnotationApplication.class, args);
+	}
+
+}
