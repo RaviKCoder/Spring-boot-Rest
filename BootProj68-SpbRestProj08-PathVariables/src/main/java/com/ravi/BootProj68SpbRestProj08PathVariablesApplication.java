4bc615b90b204fefdcc4737aa67ccd4eda22e204
Ravi kumar <rsravi433399@gmail.com>
2026-03-04 21:56:57 +0530

    Spb project9 is added

---
+++ BootProj68-SpbRestProj08-PathVariables/src/main/java/com/ravi/BootProj68SpbRestProj08PathVariablesApplication.java
@@ -0,0 +1,13 @@
+package com.ravi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootProj68SpbRestProj08PathVariablesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootProj68SpbRestProj08PathVariablesApplication.class, args);
+	}
+
+}
